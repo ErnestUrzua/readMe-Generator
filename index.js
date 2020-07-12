c8e808dfae49036ca4117b96fff3304990efa9db
ErnestUrzua <ernieurzua@gmail.com>
2020-07-11 17:59:48 -0700

    added questions to userprompt

--- index.js
+++ index.js
@@ -14,48 +14,141 @@ var fs = require("fs");
 //   * User GitHub profile picture //uploads via github 
 //   * User GitHub email
 // * At least one badge
-inquirer
-    .prompt([
+start();
+
+function start() {
+    console.log("Welcome to the \"Good\" readME generator!\n");
+    promptUser();
+}
+
+function promptUser() {
+    return inquirer.prompt([
         {
             type: "input",
             message: "What is the title?",
-            name: "Title"
+            name: "title"
         },
         {
             type: "input",
             message: "What is the description? ",
-            name: "Description"
+            name: "description"
+        },
+        {
+            type: "list",
+            name: "tableofcontents",
+            message: "Table of contents",
+            choices: ["Add a section", "Exit"]
         },
         {
             type: "input",
-            message: "Add a table of contents ",
-            name: "Table of contents"
-        },
-        {
-            type: "input",
-            message: "Installation instructions ",
-            name: "Installation"
+            message: "Input installation instructions ",
+            name: "installation"
         },
         {
             type: "input",
             message: "Usage?",
-            name: "Installation"
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "License",
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "Contributers",
+            name: "contributer"
+        },
+        {
+            type: "input",
+            message: "Test",
+            name: "test"
+        },
+        {
+            message: "upload a picture to github:",
+            name: "picture"
+        },
+        {
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            type: "confirm",
+            message: "Do you want a badge",
+            name: "badge"
         }
-    ])
-    .then(function (response) {
-        console.log(response)
+    ]).then(function (userChoice) {
+        // depending on the user selection, 
+        switch (userChoice.promptUser) {
 
-        //array to hold questions
-        const questions = [];
-    });
+            // call on a function to add a new section
+            case "Add a section":
+                addTableofContents();
+                break;
+            // exit the application by not calling on any functions
+            case "Exit":
+                console.log("\n End of table of contents");
+        }
+    })
+
+}
 
 
+//array to hold questions
+//const questions = [];
 
+//array to hold ToC
+const tableofContents = [];
+
+function initalize() {
+    try {
+        const questions = promptUser();
+
+        //const html = generateHTML(questions);
+
+        //write data to file
+        // writeToFile(readME.md, questions);
+        // console.log(Data);
+
+        //console.log("Successfully wrote to " + filename);
+        console.log(questions.title);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+//function to write data from prompt to a file
 function writeToFile(fileName, data) {
+    //CODE HERE
 }
 
-function init() {
+// define a function that lets the user add quotes to the display list
+function addTableofContents() {
+    // ask the user to input the author and then the quote
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "sectionName",
+            message: "Enter a Table of contents section: "
+        },
+        {
+            type: "input",
+            name: "sectionContent",
+            message: "Please input section content: "
+        }
+    ]).then(function (userInput) {
+        // add the new table of content section with the corresponding content into our ToC storage
+        tableofContents.push({
+            sectionName: userInput.sectionName,
+            sectionContent: userInput.sectionContent
+        });
 
+        //go back to prompt user
+        promptUser();
+    })
 }
 
-init();
+
+
+initalize();
